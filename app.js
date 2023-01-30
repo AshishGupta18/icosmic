726e41d6c05066821a68c79e535bd34a966f3700
Ashish Gupta <ashish189000@gmail.com>
2023-01-30 12:15:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 console.log("Developed with love by:")
-console.log("Moaksh Kakkar")
-console.log("Aryan Gupta")
-console.log("Rupesh Shandillya")
 console.log("Ashish Gupta")
+console.log("Aryan Gupta")
 console.log("Deepanshu Tyagi")
+console.log("Rupesh Shandillya")
+console.log("Moaksh Kakkar")
+
+
 function opentab(tabname) {
   for (tablink of tablinks) {
     tablink.classList.remove("active-link");
